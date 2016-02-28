c66fa063bd727d83c5dbb684dda989d15d696978
csterritt <csterritt@gmail.com>
2016-02-28 14:32:36 -0500

    Implemented basic Redux counter app.

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 
-const App = React.createClass({
+import { createStore } from 'redux';
+
+const Counter = React.createClass({
   render: function() {
     return (
       <div>
-        <h3>React basic app!</h3>
+        <div>Count: </div>
+        <div>
+          <button onClick={this.props.onIncrement}>+</button>
+          {this.props.count}
+          <button onClick={this.props.onDecrement}>-</button>
+        </div>
       </div>
     );
   }
 });
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('ReactApp')
-);
+const App = React.createClass({
+  render: function() {
+    const appStore = this.props.appStore;
+    return (
+      <div>
+        <h3>React basic counter app!</h3>
+        <Counter count={ appStore.getState() }
+                 onIncrement={ () =>
+                   appStore.dispatch({type: "INCREMENT"})
+                 }
+                 onDecrement={ () =>
+                   appStore.dispatch({type: "DECREMENT"})
+                 }
+        />
+      </div>
+    );
+  }
+});
+
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+const render = () => {
+  ReactDOM.render(
+    <App appStore={store}/>,
+    document.getElementById('ReactApp')
+  );
+}
+
+const store = createStore(reducer);
+store.subscribe(render);
+render();
